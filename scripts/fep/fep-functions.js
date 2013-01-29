4ad96b5423f3c1af0bc6d164c4f8aabd6e5a2d3d
Egor Kloos <egor+git@lunatech.com>
2013-01-29 17:13:21 +0100

    Removed reload and load analytics on approve

--- scripts/fep/fep-functions.js
+++ scripts/fep/fep-functions.js
@@ -143,6 +143,19 @@
     var fepCookieApproval = function($fepElements){
         
         var approved = cookie.get('approval') || false;
+                
+        var googleAnalytics = function(){
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-6087516-1']);
+            _gaq.push (['_gat._anonymizeIp']);
+            _gaq.push(['_trackPageview']);
+            
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        }
         
         if (cookie.enabled()){
             
@@ -162,26 +175,19 @@
                     
                     $(".dialog-cookies").on("click","button",function(e){
                         cookie.set("approval", "on", { expires: 4242 });
-                        location.href = location.href;
+                        googleAnalytics();
+                        $(e.delegateTarget).remove();
                     });
                     
                 } else if (approved==="on"){
                 
-                    var _gaq = _gaq || [];
-                    _gaq.push(['_setAccount', 'UA-6087516-1']);
-                    _gaq.push (['_gat._anonymizeIp']);
-                    _gaq.push(['_trackPageview']);
-                    
-                    (function() {
-                        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-                    })();
+                    googleAnalytics();
     
                 }
             }
         }
     }
+    
     
 
 
