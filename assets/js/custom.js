6e11807a04a859e23fa7713f746232a7f1e1ac9e
Nicolas Leroux <leroux.nicolas@gmail.com>
2015-09-20 17:03:22 +0200

    new website

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+/*
+  Author: bonefishcode
+  Template: CODE - Responsive Coming Soon Template
+  Version: 1.0
+  URL: http://themeforest.net/user/bonefishcode
+*/
+
+/*
+place custom function at this js file
+*/
+
+(function($) {
+  'use strict';
+
+/*=================================================
+window on load
+=================================================*/
+  $(window).on('load', function() {
+    // on load function
+  });
+
+/*=================================================
+document on ready
+=================================================*/
+  $(document).on('ready', function() {
+    // on ready function
+  });
+
+/*=================================================
+window on resize
+=================================================*/
+  $(window).on('resize', function() {
+    // on resize function
+  }).trigger('resize');
+
+})(jQuery);
